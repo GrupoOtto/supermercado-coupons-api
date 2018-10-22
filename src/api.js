6dd639f345959f46f3c1a6820f72152518e52075
Franco Borrelli <francoborrelli96@outlook.com>
2018-10-22 16:16:24 -0300

    Add valid and uses endpoints

--- src/api.js
+++ src/api.js
@@ -6,5 +6,16 @@ const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
 
 module.exports = app => {
   const resource = require('./utils/resource')(app);
+
+  app.get('/:id/valid', handle(async (req, res) => {
+    const {id} = req.params;
+    res.status(status.OK).json(await coupons.isValid(id));
+  }));
+
+  app.post('/:id/use', handle(async (req, res) => {
+    const {id} = req.params;
+    res.status(status.OK).json(await coupons.use(id));
+  }));
+
   resource('/', coupons, couponsValidator);
 };
