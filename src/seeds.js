2b4996819204ddc54acc3bafe57be10f2d8caa8f
Franco Borrelli <francoborrelli96@outlook.com>
2018-10-22 20:41:30 -0300

    Add seed files

---
+++ src/seeds.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const coupons = require('./controllers/coupons');
+
+const random = () => Math.random() >= 0.7;
+
+const Coupon = (args) => coupons.create(args)
+
+mongoose.connect(process.env.DB_URL).then(async () => {
+  for (let index = 0; index < 40; index++) {
+      const off = {
+        value: index + 1,
+        mark: random() ? '$' : '%'
+      };
+
+      const code = {
+        prefix: random() ? 'CODE-' : '',
+        postfix: random() ? '-PROMO' : ''
+      };
+
+      const uses = random() ? index : false;
+      const description = random() ? `Description of coupon ${index + 1}` : undefined;
+
+      const date = new Date;
+      const nbf = random() ? date.setDate(date.getDate() + index) : undefined;
+      const exp = random() ? date.setDate(date.getDate() + index + 1) : undefined;
+
+      const coupon = await Coupon({off, code, uses, nbf, exp, description});
+      console.log(coupon);
+  }
+});
