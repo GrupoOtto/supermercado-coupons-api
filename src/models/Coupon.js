f18661a3b5be943e74ca3ed634926d4e6f493a17
franco borrelli <francoborrelli96@outlook.com>
2018-10-21 23:13:49 -0300

    Set code as unique

--- src/models/Coupon.js
+++ src/models/Coupon.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const CouponSchema = new mongoose.Schema({
   code: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
   },
   off: {
     value: {
