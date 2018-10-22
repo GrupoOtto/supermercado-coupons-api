d630855c8ad2930beeb04c91f21a8970cf6ba835
Franco Borrelli <francoborrelli96@outlook.com>
2018-10-22 14:33:53 -0300

    Fix bug when updating coupon code

--- src/controllers/coupons.js
+++ src/controllers/coupons.js
@@ -5,12 +5,17 @@ exports.all = async (args) => Coupon.find(args);
 
 exports.get = async _id => Coupon.findById({ _id });
 
-exports.update = async (_id, args) => Coupon.findOneAndUpdate({ _id }, args);
-
 exports.delete = async _id => Coupon.deleteOne({ _id });
 
-exports.create = async args => {
-  const code = voucher_codes.generate(args.code || {});
-  return await Coupon.create({...args, code: code})
+exports.update = async (_id, args) => {
+  if (args.code){
+    args = {...args, code: voucher_codes.generate(args.code)};
+  }
+  return await Coupon.findOneAndUpdate({ _id }, args);
+};
+
+exports.create = async (args) => {
+  const voucher = voucher_codes.generate(args.code);
+  return await Coupon.create({...args, code: voucher})
 };
 
